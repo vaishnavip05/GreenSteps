d395ce5fd45029037cd3c9d42e4a07b59b8dbf73
vaishnavip05 <vaishnavip5114@gmail.com>
2026-02-12 13:26:10 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+
+def reset_user_progress():
+    """Clear session for fresh start."""
+    keys = list(st.session_state.keys())
+    for k in keys:
+        del st.session_state[k]
+
+
+def greet_user():
+    if "user" in st.session_state:
+        st.write(f"Welcome, **{st.session_state.user}** 🌱")
