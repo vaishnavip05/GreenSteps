906aed50c5b348037ea1cfd9e7c6e6ae37f52782
vaishnavip05 <vaishnavip5114@gmail.com>
2026-02-12 13:26:34 +0530

    Update ui_components.py

--- utils/ui_components.py
+++ utils/ui_components.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+
+def section_header(title):
+    st.markdown(f"## {title}")
+
+
+def info_card(text):
+    st.info(text)
+
+
+def success_card(text):
+    st.success(text)
+
+
+def divider():
+    st.divider()
