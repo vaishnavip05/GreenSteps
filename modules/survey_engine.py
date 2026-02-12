eafc8b66d7d96a9fd1ce52193341a35d7d62e118
vaishnavip05 <vaishnavip5114@gmail.com>
2026-02-12 12:38:25 +0530

    Add survey functionality with question bank

--- modules/survey_engine.py
+++ modules/survey_engine.py
@@ -1,1 +1,43 @@
+import streamlit as st
 
+
+# --------------------------------------------------
+# SAMPLE QUESTIONS (temporary)
+# later we will load from JSON
+# --------------------------------------------------
+QUESTION_BANK = {
+    "kids": [
+        "Do you switch off lights?",
+        "Do you use a water bottle?",
+    ],
+    "youth": [
+        "How do you usually travel?",
+        "Do you avoid plastic?",
+    ],
+    "adults": [
+        "Do you track electricity bills?",
+        "Are you open to home upgrades?",
+    ],
+    "elders": [
+        "Would you like to guide younger people?",
+        "Do you practice reuse habits?",
+    ],
+}
+
+
+# --------------------------------------------------
+# MAIN FUNCTION
+# --------------------------------------------------
+def run_survey(age_group):
+    st.subheader("📝 Quick Survey")
+
+    questions = QUESTION_BANK.get(age_group, [])
+    answers = {}
+
+    for q in questions:
+        answers[q] = st.radio(q, ["Yes", "Sometimes", "No"], key=q)
+
+    if st.button("Submit Survey"):
+        return answers
+
+    return None
