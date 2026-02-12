704c95adfab0663546559696f64f9a1eeba67610
vaishnavip05 <vaishnavip5114@gmail.com>
2026-02-12 14:54:29 +0530

    Update kids.py

--- age_groups/kids.py
+++ age_groups/kids.py
@@ -12,6 +12,10 @@ from games.trash_sorting import trash_sorting_ui
 def kids_dashboard():
     st.title("👶 Kids Zone")
 
+# ---------------- HOME BUTTON ----------------
+    if st.button("🏠 Home"):
+    st.session_state.page = "home"
+    st.rerun()
     # ---------------- USER INFO ----------------
     st.write(f"Welcome, **{st.session_state.user}**")
     st.write("Awareness Level:", st.session_state.user_level)
