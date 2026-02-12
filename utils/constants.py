2709a88d84361c30e48a7bbe112c959e0e693159
vaishnavip05 <vaishnavip5114@gmail.com>
2026-02-12 13:25:40 +0530

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,16 @@
+# App level constants
 
+APP_NAME = "GreenSteps"
+
+AGE_GROUPS = {
+    "kids": "5–12",
+    "youth": "13–22",
+    "adults": "23–40",
+    "elders": "40+"
+}
+
+POINTS = {
+    "small": 5,
+    "medium": 10,
+    "large": 20
+}
